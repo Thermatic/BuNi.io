544fb01ecbb3ee4be7122ed6cbe4c3608e432042
Thermatic <40235227+TheRamMaker@users.noreply.github.com>
2019-08-22 11:41:31 +1000

    Add files via upload

---
+++ Web_applet/Dashboard/js/init.js
@@ -0,0 +1,28 @@
+(function($){
+  $(function(){
+
+    $('.sidenav').sidenav();
+	   document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.dropdown-trigger');
+    var instances = M.Dropdown.init(elems, options);
+  });
+
+  // Or with jQuery
+
+  $('.dropdown-trigger').dropdown();
+	  
+	  
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('select');
+    var instances = M.FormSelect.init(elems, options);
+  });
+
+  // Or with jQuery
+
+  $(document).ready(function(){
+    $('select').formSelect();
+  });
+        
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
